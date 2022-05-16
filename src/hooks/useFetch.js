10aacf283638a7d848acb891b3679139402ad2e2
Arik Chakma <arikchangma@gmail.com>
2022-05-17 00:21:15 +0600

    Fixed React Problem

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -25,7 +25,6 @@ export default function useFetch(url, method, headers) {
     }
 
     requestFetch();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { loading, error, result };
